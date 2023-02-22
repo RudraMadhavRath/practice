fa0bd59bdee524d10bcdc57bf14d0428cff827fb
Samyak <samyakgantayat123@gmail.com>
2023-02-22 11:47:14 +0530

    JS Added

---
+++ file.js
@@ -0,0 +1,7 @@
+function SignIn(){
+       var email=document.getElementById('email_id').value;
+       var password=document.getElementById('password_id').value;
+       console.log(email);
+       console.log(password);
+       event.preventDefault();
+}
